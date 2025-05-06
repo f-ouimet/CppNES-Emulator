39ed3b0c26c65ce5dedd6064f4428aaa5ac536e4
f-ouimet <felix.ouimet@gmail.com>
2025-05-06 12:22:12 -0400

    Backup push

--- src/cpu.h
+++ src/cpu.h
@@ -31,6 +31,35 @@ private:
   void ORA_IndirectX();
   // 0x02 undefined
   void SLO_IndirectX();
+  void NOP_ZP();
+  void ORA_ZP();
+  void ASL_ZP();
+  void SLO_ZP();
+  void PHP();
+  void ORA_Immediate();
+  void ASL();
+  void ANC_Immediate();
+  void NOP_Absolute();
+  void ORA_Absolute();
+  void ASL_Absolute();
+  void SLO_Absolute();
+
+  // 0x10 to 0x1F
+  void BPL();
+  void ORA_IndirectY();
+  // empty
+  void SLO_IndirectY();
+  void NOP_ZPX();
+  void ORA_ZPX();
+  void ASL_ZPX();
+  void SLO_ZPX();
+  void CLC();
+  void ORA_AbsoluteY();
+  void SLO_AbsoluteY();
+  void NOP_AbsoluteX();
+  void ORA_AbsoluteX();
+  void ASL_AbsoluteX();
+  void SLO_AbsoluteX();
 
   // Instruction table
   std::array<std::function<void()>, 256> instructionTable;
