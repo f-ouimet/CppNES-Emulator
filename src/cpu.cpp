a94131120f0242d6d62c3e679d55f02ff493daed
f-ouimet <felix.ouimet@gmail.com>
2025-05-08 11:03:22 -0400

    added all instructions

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -7,4 +7,293 @@
 CPU::CPU() {
   instructionTable.fill(
       [this]() { this->NOP(); }); // for unknown opcodes, default to NOP
+  // 0x00 to 0x0F : Checked Content
+  instructionTable[0x00] = [this]() { this->BRK(); };
+  instructionTable[0x01] = [this]() { this->ORA_IndirectX(); };
+  instructionTable[0x02] = [this]() { this->STP(); };
+  instructionTable[0x03] = [this]() { this->SLO_IndirectX(); };
+  instructionTable[0x04] = [this]() { this->NOP_ZP(); };
+  instructionTable[0x05] = [this]() { this->ORA_ZP(); };
+  instructionTable[0x06] = [this]() { this->ASL_ZP(); };
+  instructionTable[0x07] = [this]() { this->SLO_ZP(); };
+  instructionTable[0x08] = [this]() { this->PHP(); };
+  instructionTable[0x09] = [this]() { this->ORA_Immediate(); };
+  instructionTable[0x0A] = [this]() { this->ASL(); };
+  instructionTable[0x0B] = [this]() { this->ANC_Immediate(); };
+  instructionTable[0x0C] = [this]() { this->NOP_Absolute(); };
+  instructionTable[0x0D] = [this]() { this->ORA_Absolute(); };
+  instructionTable[0x0E] = [this]() { this->ASL_Absolute(); };
+  instructionTable[0x0F] = [this]() { this->SLO_Absolute(); };
+
+  // 0x10 - 0x1F
+  instructionTable[0x10] = [this]() { this->BPL(); };
+  instructionTable[0x11] = [this]() { this->ORA_IndirectY(); };
+  instructionTable[0x12] = [this]() { this->STP(); };
+  instructionTable[0x13] = [this]() { this->SLO_IndirectY(); };
+  instructionTable[0x14] = [this]() { this->NOP_ZPX(); };
+  instructionTable[0x15] = [this]() { this->ORA_ZPX(); };
+  instructionTable[0x16] = [this]() { this->ASL_ZPX(); };
+  instructionTable[0x17] = [this]() { this->SLO_ZPX(); };
+  instructionTable[0x18] = [this]() { this->CLC(); };
+  instructionTable[0x19] = [this]() { this->ORA_AbsoluteY(); };
+  instructionTable[0x1A] = [this]() { this->NOP(); };
+  instructionTable[0x1B] = [this]() { this->SLO_AbsoluteY(); };
+  instructionTable[0x1C] = [this]() { this->NOP_AbsoluteX(); };
+  instructionTable[0x1D] = [this]() { this->ORA_AbsoluteX(); };
+  instructionTable[0x1E] = [this]() { this->ASL_AbsoluteX(); };
+  instructionTable[0x1F] = [this]() { this->SLO_AbsoluteX(); };
+
+  // 0x20 - 0x2F
+  instructionTable[0x20] = [this]() { this->JSR_Absolute(); };
+  instructionTable[0x21] = [this]() { this->AND_IndirectX(); };
+  instructionTable[0x22] = [this]() { this->NOP(); };
+  instructionTable[0x23] = [this]() { this->RLA_IndirectX(); };
+  instructionTable[0x24] = [this]() { this->BIT_ZP(); };
+  instructionTable[0x25] = [this]() { this->AND_ZP(); };
+  instructionTable[0x26] = [this]() { this->ROL_ZP(); };
+  instructionTable[0x27] = [this]() { this->RLA_ZP(); };
+  instructionTable[0x28] = [this]() { this->PLP(); };
+  instructionTable[0x29] = [this]() { this->AND_Immediate(); };
+  instructionTable[0x2A] = [this]() { this->ROL(); };
+  instructionTable[0x2B] = [this]() { this->ANC_Immediate(); };
+  instructionTable[0x2C] = [this]() { this->BIT_Absolute(); };
+  instructionTable[0x2D] = [this]() { this->AND_Absolute(); };
+  instructionTable[0x2E] = [this]() { this->ROL_Absolute(); };
+  instructionTable[0x2F] = [this]() { this->RLA_Absolute(); };
+
+  // 0x30 - 0x3F
+  instructionTable[0x30] = [this]() { this->BMI(); };
+  instructionTable[0x31] = [this]() { this->AND_IndirectY(); };
+  instructionTable[0x32] = [this]() { this->NOP(); };
+  instructionTable[0x33] = [this]() { this->RLA_IndirectY(); };
+  instructionTable[0x34] = [this]() { this->NOP_ZPX(); };
+  instructionTable[0x35] = [this]() { this->AND_ZPX(); };
+  instructionTable[0x36] = [this]() { this->ROL_ZPX(); };
+  instructionTable[0x37] = [this]() { this->RLA_ZPX(); };
+  instructionTable[0x38] = [this]() { this->SEC(); };
+  instructionTable[0x39] = [this]() { this->AND_AbsoluteY(); };
+  instructionTable[0x3A] = [this]() { this->NOP(); };
+  instructionTable[0x3B] = [this]() { this->RLA_AbsoluteY(); };
+  instructionTable[0x3C] = [this]() { this->NOP_AbsoluteX(); };
+  instructionTable[0x3D] = [this]() { this->AND_AbsoluteX(); };
+  instructionTable[0x3E] = [this]() { this->ROL_AbsoluteX(); };
+  instructionTable[0x3F] = [this]() { this->RLA_AbsoluteX(); };
+
+  // 0x40 - 0x4F
+  instructionTable[0x40] = [this]() { this->RTI(); };
+  instructionTable[0x41] = [this]() { this->EOR_IndirectX(); };
+  instructionTable[0x42] = [this]() { this->NOP(); };
+  instructionTable[0x43] = [this]() { this->SRE_IndirectX(); };
+  instructionTable[0x44] = [this]() { this->NOP_ZP(); };
+  instructionTable[0x45] = [this]() { this->EOR_ZP(); };
+  instructionTable[0x46] = [this]() { this->LSR_ZP(); };
+  instructionTable[0x47] = [this]() { this->SRE_ZP(); };
+  instructionTable[0x48] = [this]() { this->PHA(); };
+  instructionTable[0x49] = [this]() { this->EOR_Immediate(); };
+  instructionTable[0x4A] = [this]() { this->LSR(); };
+  instructionTable[0x4B] = [this]() { this->ALR_Immediate(); };
+  instructionTable[0x4C] = [this]() { this->JMP_Absolute(); };
+  instructionTable[0x4D] = [this]() { this->EOR_Absolute(); };
+  instructionTable[0x4E] = [this]() { this->LSR_Absolute(); };
+  instructionTable[0x4F] = [this]() { this->SRE_Absolute(); };
+
+  // 0x50 - 0x5F
+  instructionTable[0x50] = [this]() { this->BVC(); };
+  instructionTable[0x51] = [this]() { this->EOR_IndirectY(); };
+  instructionTable[0x52] = [this]() { this->NOP(); };
+  instructionTable[0x53] = [this]() { this->SRE_IndirectY(); };
+  instructionTable[0x54] = [this]() { this->NOP_ZPX(); };
+  instructionTable[0x55] = [this]() { this->EOR_ZPX(); };
+  instructionTable[0x56] = [this]() { this->LSR_ZPX(); };
+  instructionTable[0x57] = [this]() { this->SRE_ZPX(); };
+  instructionTable[0x58] = [this]() { this->CLI(); };
+  instructionTable[0x59] = [this]() { this->EOR_AbsoluteY(); };
+  instructionTable[0x5A] = [this]() { this->NOP(); };
+  instructionTable[0x5B] = [this]() { this->SRE_AbsoluteY(); };
+  instructionTable[0x5C] = [this]() { this->NOP_AbsoluteX(); };
+  instructionTable[0x5D] = [this]() { this->EOR_AbsoluteX(); };
+  instructionTable[0x5E] = [this]() { this->LSR_AbsoluteX(); };
+  instructionTable[0x5F] = [this]() { this->SRE_AbsoluteX(); };
+
+  // 0x60 - 0x6F
+  instructionTable[0x60] = [this]() { this->RTS(); };
+  instructionTable[0x61] = [this]() { this->ADC_IndirectX(); };
+  instructionTable[0x62] = [this]() { this->NOP(); };
+  instructionTable[0x63] = [this]() { this->RRA_IndirectX(); };
+  instructionTable[0x64] = [this]() { this->NOP_ZP(); };
+  instructionTable[0x65] = [this]() { this->ADC_ZP(); };
+  instructionTable[0x66] = [this]() { this->ROR_ZP(); };
+  instructionTable[0x67] = [this]() { this->RRA_ZP(); };
+  instructionTable[0x68] = [this]() { this->PLA(); };
+  instructionTable[0x69] = [this]() { this->ADC_Immediate(); };
+  instructionTable[0x6A] = [this]() { this->ROR(); };
+  instructionTable[0x6B] = [this]() { this->ARR_Immediate(); };
+  instructionTable[0x6C] = [this]() { this->JMP_Indirect(); };
+  instructionTable[0x6D] = [this]() { this->ADC_Absolute(); };
+  instructionTable[0x6E] = [this]() { this->ROR_Absolute(); };
+  instructionTable[0x6F] = [this]() { this->RRA_Absolute(); };
+
+  // 0x70 - 0x7F
+  instructionTable[0x70] = [this]() { this->BVS(); };
+  instructionTable[0x71] = [this]() { this->ADC_IndirectY(); };
+  instructionTable[0x72] = [this]() { this->NOP(); };
+  instructionTable[0x73] = [this]() { this->RRA_IndirectY(); };
+  instructionTable[0x74] = [this]() { this->NOP_ZPX(); };
+  instructionTable[0x75] = [this]() { this->ADC_ZPX(); };
+  instructionTable[0x76] = [this]() { this->ROR_ZPX(); };
+  instructionTable[0x77] = [this]() { this->RRA_ZPX(); };
+  instructionTable[0x78] = [this]() { this->SEI(); };
+  instructionTable[0x79] = [this]() { this->ADC_AbsoluteY(); };
+  instructionTable[0x7A] = [this]() { this->NOP(); };
+  instructionTable[0x7B] = [this]() { this->RRA_AbsoluteY(); };
+  instructionTable[0x7C] = [this]() { this->NOP_AbsoluteX(); };
+  instructionTable[0x7D] = [this]() { this->ADC_AbsoluteX(); };
+  instructionTable[0x7E] = [this]() { this->ROR_AbsoluteX(); };
+  instructionTable[0x7F] = [this]() { this->RRA_AbsoluteX(); };
+
+  // 0x80 - 0x8F
+  instructionTable[0x80] = [this]() { this->NOP_Immediate(); };
+  instructionTable[0x81] = [this]() { this->STA_IndirectX(); };
+  instructionTable[0x82] = [this]() { this->NOP(); };
+  instructionTable[0x83] = [this]() { this->SAX_IndirectX(); };
+  instructionTable[0x84] = [this]() { this->STY_ZP(); };
+  instructionTable[0x85] = [this]() { this->STA_ZP(); };
+  instructionTable[0x86] = [this]() { this->STX_ZP(); };
+  instructionTable[0x87] = [this]() { this->SAX_ZP(); };
+  instructionTable[0x88] = [this]() { this->DEY(); };
+  instructionTable[0x89] = [this]() { this->NOP(); };
+  instructionTable[0x8A] = [this]() { this->TXA(); };
+  instructionTable[0x8B] = [this]() { this->XAA_Immediate(); };
+  instructionTable[0x8C] = [this]() { this->STY_Absolute(); };
+  instructionTable[0x8D] = [this]() { this->STA_Absolute(); };
+  instructionTable[0x8E] = [this]() { this->STX_Absolute(); };
+  instructionTable[0x8F] = [this]() { this->SAX_Absolute(); };
+
+  // 0x90 - 0x9F
+  instructionTable[0x90] = [this]() { this->BCC(); };
+  instructionTable[0x91] = [this]() { this->STA_IndirectY(); };
+  instructionTable[0x92] = [this]() { this->NOP(); };
+  instructionTable[0x93] = [this]() { this->AHX_IndirectY(); };
+  instructionTable[0x94] = [this]() { this->STY_ZPX(); };
+  instructionTable[0x95] = [this]() { this->STA_ZPX(); };
+  instructionTable[0x96] = [this]() { this->STX_ZPY(); };
+  instructionTable[0x97] = [this]() { this->SAX_ZPY(); };
+  instructionTable[0x98] = [this]() { this->TYA(); };
+  instructionTable[0x99] = [this]() { this->STA_AbsoluteY(); };
+  instructionTable[0x9A] = [this]() { this->TXS(); };
+  instructionTable[0x9B] = [this]() { this->TAS_AbsoluteY(); };
+  instructionTable[0x9C] = [this]() { this->SHY_AbsoluteX(); };
+  instructionTable[0x9D] = [this]() { this->STA_AbsoluteX(); };
+  instructionTable[0x9E] = [this]() { this->SHX_AbsoluteY(); };
+  instructionTable[0x9F] = [this]() { this->AHX_AbsoluteY(); };
+
+  // 0xA0 - 0xAF
+  instructionTable[0xA0] = [this]() { this->LDY_Immediate(); };
+  instructionTable[0xA1] = [this]() { this->LDA_IndirectX(); };
+  instructionTable[0xA2] = [this]() { this->LDX_Immediate(); };
+  instructionTable[0xA3] = [this]() { this->LAX_IndirectX(); };
+  instructionTable[0xA4] = [this]() { this->LDY_ZP(); };
+  instructionTable[0xA5] = [this]() { this->LDA_ZP(); };
+  instructionTable[0xA6] = [this]() { this->LDX_ZP(); };
+  instructionTable[0xA7] = [this]() { this->LAX_ZP(); };
+  instructionTable[0xA8] = [this]() { this->TAY(); };
+  instructionTable[0xA9] = [this]() { this->LDA_Immediate(); };
+  instructionTable[0xAA] = [this]() { this->TAX(); };
+  instructionTable[0xAB] = [this]() { this->LAX_Immediate(); };
+  instructionTable[0xAC] = [this]() { this->LDY_Absolute(); };
+  instructionTable[0xAD] = [this]() { this->LDA_Absolute(); };
+  instructionTable[0xAE] = [this]() { this->LDX_Absolute(); };
+  instructionTable[0xAF] = [this]() { this->LAX_Absolute(); };
+
+  // 0xB0 - 0xBF
+  instructionTable[0xB0] = [this]() { this->BCS(); };
+  instructionTable[0xB1] = [this]() { this->LDA_IndirectY(); };
+  instructionTable[0xB2] = [this]() { this->NOP(); };
+  instructionTable[0xB3] = [this]() { this->LAX_IndirectY(); };
+  instructionTable[0xB4] = [this]() { this->LDY_ZPX(); };
+  instructionTable[0xB5] = [this]() { this->LDA_ZPX(); };
+  instructionTable[0xB6] = [this]() { this->LDX_ZPY(); };
+  instructionTable[0xB7] = [this]() { this->LAX_ZPY(); };
+  instructionTable[0xB8] = [this]() { this->CLV(); };
+  instructionTable[0xB9] = [this]() { this->LDA_AbsoluteY(); };
+  instructionTable[0xBA] = [this]() { this->TSX(); };
+  instructionTable[0xBB] = [this]() { this->LAS_AbsoluteY(); };
+  instructionTable[0xBC] = [this]() { this->LDY_AbsoluteX(); };
+  instructionTable[0xBD] = [this]() { this->LDA_AbsoluteX(); };
+  instructionTable[0xBE] = [this]() { this->LDX_AbsoluteY(); };
+  instructionTable[0xBF] = [this]() { this->LAX_AbsoluteY(); };
+
+  // 0xC0 - 0xCF
+  instructionTable[0xC0] = [this]() { this->CPY_Immediate(); };
+  instructionTable[0xC1] = [this]() { this->CMP_IndirectX(); };
+  instructionTable[0xC2] = [this]() { this->NOP(); };
+  instructionTable[0xC3] = [this]() { this->DCP_IndirectX(); };
+  instructionTable[0xC4] = [this]() { this->CPY_ZP(); };
+  instructionTable[0xC5] = [this]() { this->CMP_ZP(); };
+  instructionTable[0xC6] = [this]() { this->DEC_ZP(); };
+  instructionTable[0xC7] = [this]() { this->DCP_ZP(); };
+  instructionTable[0xC8] = [this]() { this->INY(); };
+  instructionTable[0xC9] = [this]() { this->CMP_Immediate(); };
+  instructionTable[0xCA] = [this]() { this->DEX(); };
+  instructionTable[0xCB] = [this]() { this->AXS_Immediate(); };
+  instructionTable[0xCC] = [this]() { this->CPY_Absolute(); };
+  instructionTable[0xCD] = [this]() { this->CMP_Absolute(); };
+  instructionTable[0xCE] = [this]() { this->DEC_Absolute(); };
+  instructionTable[0xCF] = [this]() { this->DCP_Absolute(); };
+
+  // 0xD0 - 0xDF
+  instructionTable[0xD0] = [this]() { this->BNE(); };
+  instructionTable[0xD1] = [this]() { this->CMP_IndirectY(); };
+  instructionTable[0xD2] = [this]() { this->NOP(); };
+  instructionTable[0xD3] = [this]() { this->DCP_IndirectY(); };
+  instructionTable[0xD4] = [this]() { this->NOP_ZPX(); };
+  instructionTable[0xD5] = [this]() { this->CMP_ZPX(); };
+  instructionTable[0xD6] = [this]() { this->DEC_ZPX(); };
+  instructionTable[0xD7] = [this]() { this->DCP_ZPX(); };
+  instructionTable[0xD8] = [this]() { this->CLD(); };
+  instructionTable[0xD9] = [this]() { this->CMP_AbsoluteY(); };
+  instructionTable[0xDA] = [this]() { this->NOP(); };
+  instructionTable[0xDB] = [this]() { this->DCP_AbsoluteY(); };
+  instructionTable[0xDC] = [this]() { this->NOP_AbsoluteX(); };
+  instructionTable[0xDD] = [this]() { this->CMP_AbsoluteX(); };
+  instructionTable[0xDE] = [this]() { this->DEC_AbsoluteX(); };
+  instructionTable[0xDF] = [this]() { this->DCP_AbsoluteX(); };
+
+  // 0xE0 - 0xEF
+  instructionTable[0xE0] = [this]() { this->CPX_Immediate(); };
+  instructionTable[0xE1] = [this]() { this->SBC_IndirectX(); };
+  instructionTable[0xE2] = [this]() { this->NOP(); };
+  instructionTable[0xE3] = [this]() { this->ISC_IndirectX(); };
+  instructionTable[0xE4] = [this]() { this->CPX_ZP(); };
+  instructionTable[0xE5] = [this]() { this->SBC_ZP(); };
+  instructionTable[0xE6] = [this]() { this->INC_ZP(); };
+  instructionTable[0xE7] = [this]() { this->ISC_ZP(); };
+  instructionTable[0xE8] = [this]() { this->INX(); };
+  instructionTable[0xE9] = [this]() { this->SBC_Immediate(); };
+  instructionTable[0xEA] = [this]() { this->NOP(); };
+  instructionTable[0xEB] = [this]() { this->SBC_Immediate(); };
+  instructionTable[0xEC] = [this]() { this->CPX_Absolute(); };
+  instructionTable[0xED] = [this]() { this->SBC_Absolute(); };
+  instructionTable[0xEE] = [this]() { this->INC_Absolute(); };
+  instructionTable[0xEF] = [this]() { this->ISC_Absolute(); };
+
+  // 0xF0 - 0xFF
+  instructionTable[0xF0] = [this]() { this->BEQ(); };
+  instructionTable[0xF1] = [this]() { this->SBC_IndirectY(); };
+  instructionTable[0xF2] = [this]() { this->NOP(); };
+  instructionTable[0xF3] = [this]() { this->ISC_IndirectY(); };
+  instructionTable[0xF4] = [this]() { this->NOP_ZPX(); };
+  instructionTable[0xF5] = [this]() { this->SBC_ZPX(); };
+  instructionTable[0xF6] = [this]() { this->INC_ZPX(); };
+  instructionTable[0xF7] = [this]() { this->ISC_ZPX(); };
+  instructionTable[0xF8] = [this]() { this->SED(); };
+  instructionTable[0xF9] = [this]() { this->SBC_AbsoluteY(); };
+  instructionTable[0xFA] = [this]() { this->NOP(); };
+  instructionTable[0xFB] = [this]() { this->ISC_AbsoluteY(); };
+  instructionTable[0xFC] = [this]() { this->NOP_AbsoluteX(); };
+  instructionTable[0xFD] = [this]() { this->SBC_AbsoluteX(); };
+  instructionTable[0xFE] = [this]() { this->INC_AbsoluteX(); };
+  instructionTable[0xFF] = [this]() { this->ISC_AbsoluteX(); };
 }
+
+void BRK() { return; }
--- src/cpu.h
+++ src/cpu.h
@@ -7,29 +7,50 @@
 
 class CPU {
 private:
-  uint8_t SP;
   uint16_t PC;
+  uint16_t SP; // upper 8bits are fixed to 00000001
+
+  uint8_t accumulator;
+  uint8_t processorStatusReg;
+  uint8_t xIndex;
+  uint8_t yIndex;
+
+  uint8_t mem[65535]; // address space 0x0000 to 0xFFFF
+
+  // zero page goes from 0 to 00FF
+  // system stack is 0100 to 01FF
+  // Last 6 bytes are reserved
+  // FFFA to FFFF is interrupt handler for A and B, power on reset for C and D,
+  // BRK/Interrupt request handler E and F
+
   void executeInstruction(uint8_t opcode);
+  // TODO:load rom in main or from CPU, where is ROM stored when execd?
+  // void loadROM();
 
   // all instructions listed in row order from
   // https://llx.com/Neil/a2/opcodes.html
   // and https://nesdev.org/wiki/CPU_unofficial_opcodes
   // we use ALL opcodes from the base 6502
   //
-  // #i = immediate addr
+  // https://www.nesdev.org/wiki/CPU_addressing_modes
+  // #i or #v = immediate addr
   // a = absolute 16 bit addr
+  // (a) = indirect
   // a,x abs x
   // a,y abs y
   // d = 8 bit zero page addr
-  // d,x = zero page X
-  // (d,x) = Indirect_X
-  // (d),y = Indirect_Y
+  // d,x = zero page indexed X
+  // d,y = Zero page indexed Y
+  // (d,x) = Indirect_X  (indexed indirect)
+  // (d),y = Indirect_Y  (indirect indexed)
+  // A = accumulator
+  // label = Relative
 
   // instruction functions
   // 0x00 to 0x1F
   void BRK();
   void ORA_IndirectX();
-  // 0x02 undefined
+  void STP();
   void SLO_IndirectX();
   void NOP_ZP();
   void ORA_ZP();
@@ -47,7 +68,7 @@ private:
   // 0x10 to 0x1F
   void BPL();
   void ORA_IndirectY();
-  // empty
+  // empty (STP)
   void SLO_IndirectY();
   void NOP_ZPX();
   void ORA_ZPX();
@@ -55,11 +76,262 @@ private:
   void SLO_ZPX();
   void CLC();
   void ORA_AbsoluteY();
+  void NOP();
   void SLO_AbsoluteY();
   void NOP_AbsoluteX();
   void ORA_AbsoluteX();
   void ASL_AbsoluteX();
   void SLO_AbsoluteX();
+
+  // 0x20 to 0x2F
+  void JSR_Absolute();
+  void AND_IndirectX();
+  // empty
+  void RLA_IndirectX();
+  void BIT_ZP();
+  void AND_ZP();
+  void ROL_ZP();
+  void RLA_ZP();
+  void PLP();
+  void AND_Immediate();
+  void ROL();
+  // duplicate ANC_Immediate
+  void BIT_Absolute();
+  void AND_Absolute();
+  void ROL_Absolute();
+  void RLA_Absolute();
+  // 0x30 to 0x3F
+  void BMI();
+  void AND_IndirectY();
+  // empty
+  void RLA_IndirectY();
+  // duplicate NOP_ZPX
+  void AND_ZPX();
+  void ROL_ZPX();
+  void RLA_ZPX();
+  void SEC();
+  void AND_AbsoluteY();
+  // duped NOP
+  void RLA_AbsoluteY();
+  // duped NOP_AbsX
+  void AND_AbsoluteX();
+  void ROL_AbsoluteX();
+  void RLA_AbsoluteX();
+  //  0x40 to 0x4F
+  void RTI();
+  void EOR_IndirectX();
+  // empty
+  void SRE_IndirectX();
+  // void NOP_ZP();
+  void EOR_ZP();
+  void LSR_ZP();
+  void SRE_ZP();
+  void PHA();
+  void EOR_Immediate();
+  void LSR();
+  void ALR_Immediate();
+  void JMP_Absolute();
+  void EOR_Absolute();
+  void LSR_Absolute();
+  void SRE_Absolute();
+
+  // 0x50 to 0x5F
+  void BVC();
+  void EOR_IndirectY();
+  // undefined
+  void SRE_IndirectY();
+  // void NOP_ZPX();
+  void EOR_ZPX();
+  void LSR_ZPX();
+  void SRE_ZPX();
+  void CLI();
+  void EOR_AbsoluteY();
+  // void NOP();
+  void SRE_AbsoluteY();
+  // void NOP_AbsoluteX();
+  void EOR_AbsoluteX();
+  void LSR_AbsoluteX();
+  void SRE_AbsoluteX();
+
+  // 0x60 to 0x6F
+  void RTS();
+  void ADC_IndirectX();
+  // undefined
+  void RRA_IndirectX();
+  // void NOP_ZP();
+  void ADC_ZP();
+  void ROR_ZP();
+  void RRA_ZP();
+  void PLA();
+  void ADC_Immediate();
+  void ROR();
+  void ARR_Immediate();
+  void JMP_Indirect();
+  void ADC_Absolute();
+  void ROR_Absolute();
+  void RRA_Absolute();
+
+  // 0x70 to 0x7F
+  void BVS();
+  void ADC_IndirectY();
+  // undefined
+  void RRA_IndirectY();
+  // void NOP_ZPX();
+  void ADC_ZPX();
+  void ROR_ZPX();
+  void RRA_ZPX();
+  void SEI();
+  void ADC_AbsoluteY();
+  // void NOP();
+  void RRA_AbsoluteY();
+  // void NOP_AbsoluteX();
+  void ADC_AbsoluteX();
+  void ROR_AbsoluteX();
+  void RRA_AbsoluteX();
+
+  // 0x80 to 0x8F
+  void NOP_Immediate();
+  void STA_IndirectX();
+  // void NOP_Immediate_2(); // same as 0x80 (undocumented), used for alias
+  void SAX_IndirectX();
+  void STY_ZP();
+  void STA_ZP();
+  void STX_ZP();
+  void SAX_ZP();
+  void DEY();
+  // void NOP_Immediate_3(); // same as 0x89
+  void TXA();
+  void XAA_Immediate();
+  void STY_Absolute();
+  void STA_Absolute();
+  void STX_Absolute();
+  void SAX_Absolute();
+
+  // 0x90 to 0x9F
+  void BCC();
+  void STA_IndirectY();
+  // undefined
+  void AHX_IndirectY();
+  void STY_ZPX();
+  void STA_ZPX();
+  void STX_ZPY();
+  void SAX_ZPY(); // unofficial
+  void TYA();
+  void STA_AbsoluteY();
+  void TXS();
+  void TAS_AbsoluteY();
+  void SHY_AbsoluteX();
+  void STA_AbsoluteX();
+  void SHX_AbsoluteY();
+  void AHX_AbsoluteY();
+
+  // 0xA0 to 0xAF
+  void LDY_Immediate();
+  void LDA_IndirectX();
+  void LDX_Immediate();
+  void LAX_IndirectX();
+  void LDY_ZP();
+  void LDA_ZP();
+  void LDX_ZP();
+  void LAX_ZP();
+  void TAY();
+  void LDA_Immediate();
+  void TAX();
+  void LAX_Immediate();
+  void LDY_Absolute();
+  void LDA_Absolute();
+  void LDX_Absolute();
+  void LAX_Absolute();
+
+  // 0xB0 to 0xBF
+  void BCS();
+  void LDA_IndirectY();
+  // undefined
+  void LAX_IndirectY();
+  void LDY_ZPX();
+  void LDA_ZPX();
+  void LDX_ZPY();
+  void LAX_ZPY();
+  void CLV();
+  void LDA_AbsoluteY();
+  void TSX();
+  void LAS_AbsoluteY();
+  void LDY_AbsoluteX();
+  void LDA_AbsoluteX();
+  void LDX_AbsoluteY();
+  void LAX_AbsoluteY();
+
+  // 0xC0 to 0xCF
+  void CPY_Immediate();
+  void CMP_IndirectX();
+  // undefined
+  void DCP_IndirectX();
+  void CPY_ZP();
+  void CMP_ZP();
+  void DEC_ZP();
+  void DCP_ZP();
+  void INY();
+  void CMP_Immediate();
+  void DEX();
+  void AXS_Immediate();
+  void CPY_Absolute();
+  void CMP_Absolute();
+  void DEC_Absolute();
+  void DCP_Absolute();
+
+  // 0xD0 to 0xDF
+  void BNE();
+  void CMP_IndirectY();
+  // undefined
+  void DCP_IndirectY();
+  // void NOP_ZPX();
+  void CMP_ZPX();
+  void DEC_ZPX();
+  void DCP_ZPX();
+  void CLD();
+  void CMP_AbsoluteY();
+  // void NOP();
+  void DCP_AbsoluteY();
+  // void NOP_AbsoluteX();
+  void CMP_AbsoluteX();
+  void DEC_AbsoluteX();
+  void DCP_AbsoluteX();
+
+  // 0xE0 to 0xEF
+  void CPX_Immediate();
+  void SBC_IndirectX();
+  // undefined
+  void ISC_IndirectX();
+  void CPX_ZP();
+  void SBC_ZP();
+  void INC_ZP();
+  void ISC_ZP();
+  void INX();
+  void SBC_Immediate();
+  // void NOP_Immediate();
+  //  void SBC_Immediate_2(); // unofficial ALR/ISC style op
+  void CPX_Absolute();
+  void SBC_Absolute();
+  void INC_Absolute();
+  void ISC_Absolute();
+
+  // 0xF0 to 0xFF
+  void BEQ();
+  void SBC_IndirectY();
+  // undefined
+  void ISC_IndirectY();
+  // void NOP_ZPX();
+  void SBC_ZPX();
+  void INC_ZPX();
+  void ISC_ZPX();
+  void SED();
+  void SBC_AbsoluteY();
+  // void NOP();
+  void ISC_AbsoluteY();
+  // void NOP_AbsoluteX();
+  void SBC_AbsoluteX();
+  void INC_AbsoluteX();
+  void ISC_AbsoluteX();
 
   // Instruction table
   std::array<std::function<void()>, 256> instructionTable;
